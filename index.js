6a335fcf7519f93b63bae311eb410d86574e46a8
Emma Konrad <emma.konrad92@gmail.com>
2017-09-09 00:40:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n++
+}
+
+function dec(n) {
+  return n--
+ }
+function makeInt(n) {
+  return parseInt('9.18', 10)
+}
+
+function makeInt(n) {
+  return parseInt('0', 10)
+}
